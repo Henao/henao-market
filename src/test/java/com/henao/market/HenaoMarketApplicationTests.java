4c6a87cc740226e2cec9d374da5b55b02f0d64e9
Julian Henao Henao <jhenaohenao@gmail.com>
2021-01-18 11:33:44 -0500

    First commit in java spring project - initial project configuration API

---
+++ src/test/java/com/henao/market/HenaoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henao.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HenaoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
