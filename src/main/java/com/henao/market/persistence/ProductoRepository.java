ebdd59267e4b8f7fe2341abdc131de493d98cabc
Julian Henao Henao <jhenaohenao@gmail.com>
2021-01-20 12:42:28 -0500

    ProductRepository terminado, uso de query methods

--- src/main/java/com/henao/market/persistence/ProductoRepository.java
+++ src/main/java/com/henao/market/persistence/ProductoRepository.java
@@ -2,13 +2,43 @@ package com.henao.market.persistence;
 
 import com.henao.market.persistence.crud.ProductoCrudRepository;
 import com.henao.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
+@Repository
+// Se puede usar la anotación Component pero esa es más general
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
+// método público que retorna lista de productos
     public List<Producto> getAll(){
+
         return (List<Producto>) productoCrudRepository.findAll();
     }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public  Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+
+
+
 }
--- src/main/java/com/henao/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/henao/market/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,20 @@
 package com.henao.market.persistence.crud;
 
 import com.henao.market.persistence.entity.Producto;
+
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
+//Spring Repository recibe dos parametros, la tabla y el typo de la clave primaria
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+//    Si usamos una anotación @Query Nativo no es necesario llamar el método asi: findByIdCategoria podría llamarse de cualquier manera getByCategoria
+//    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+
+//    Usar Query methodses mejor práctica pues ofrecen más flexibilidad
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+  Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
